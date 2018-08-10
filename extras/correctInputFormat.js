f3610a5d8a455649d2926b84370c67d8ec1f207a
shanurrahman <rshanur@saintsoftwareinc.com>
2018-08-10 05:07:55 -0700

    Added formatters to convert input Objects fields to required format to store in the database

---
+++ extras/correctInputFormat.js
@@ -0,0 +1,55 @@
+let arr = [ {
+    "Rule": "ProductionFormationDataCheck",
+    "Tests": "NoSpecialCharacters, NoNumbers",
+    "Score": "Mean"
+  },
+  {
+    "Rule": "PrimaryDirectionDataCheck",
+    "Tests": "NoSpaces, NoSpecialCharacters, SetValues_PreDefined1",
+    "Score": "Sum"
+  },
+  {
+    "Rule": "APINumberDataCheck",
+    "Tests": "NumberOnly, FixedLength, NonNegative",
+    "Score": "Sum"
+  },
+  {
+    "Rule": "PrimaryMaterialDataCheck",
+    "Tests": "NoSpecialCharacters, SetValues_PreDefined2",
+    "Score": "Mean"
+  },
+  {
+    "Rule": "GUWIDNonNullCheck",
+    "Tests": "NumberOnly, NonNegative, NonNull, NonDecimal",
+    "Score": "Lowest"
+  },
+  {
+    "Rule": "GenericStringCheck",
+    "Tests": "NonNumber, NoSpecialCharacters",
+    "Score": "Sum"
+  },
+  {
+    "Rule": "LattitudeDataCheck",
+    "Tests": "NoSpecialCharacters, MaxLength1, SetRange_PreDefined1",
+    "Score": "Lowest"
+  },
+  {
+    "Rule": "LongitudeDataCheck",
+    "Tests": "NoSpecialCharacters, MaxLength2, SetRange_PreDefined2",
+    "Score": "Lowest"
+  }
+]
+
+format = function(arr){
+    return arr.split(",");
+}
+
+
+// x is one object from the array at a time, but x is not iterable so we can use Object.keys()
+// you have to return the modified array element, so that will be the object inside the array, which in this case is the modified product
+arr.map((product, index)=>{
+    product["Tests"] = format(product["Tests"])
+})
+
+
+console.log(arr)
